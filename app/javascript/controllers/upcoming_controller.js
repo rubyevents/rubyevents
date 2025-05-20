f9792e6faf0733e58deb433fe0cd62ffe7bc8568
Marco Roth <marco.roth@intergga.ch>
2025-05-20 21:22:21 +0200

    Lint

--- app/javascript/controllers/upcoming_controller.js
+++ app/javascript/controllers/upcoming_controller.js
@@ -1,26 +1,26 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["item", "poster"]
+  static targets = ['item', 'poster']
 
-  connect() {
+  connect () {
     const firstEvent = this.itemTargets[0]
 
-    this.posterTargetFor(firstEvent.dataset.eventId)?.classList.remove("hidden")
+    this.posterTargetFor(firstEvent.dataset.eventId)?.classList.remove('hidden')
   }
 
-  reveal(event) {
-    const eventId = event.target.closest(".event-item").dataset.eventId
+  reveal (event) {
+    const eventId = event.target.closest('.event-item').dataset.eventId
 
     this.hidePosters()
-    this.posterTargetFor(eventId)?.classList.remove("hidden")
+    this.posterTargetFor(eventId)?.classList.remove('hidden')
   }
 
-  hidePosters() {
-    this.posterTargets.forEach(poster => poster.classList.add("hidden"))
+  hidePosters () {
+    this.posterTargets.forEach(poster => poster.classList.add('hidden'))
   }
 
-  posterTargetFor(eventId) {
+  posterTargetFor (eventId) {
     return this.posterTargets.find(poster => poster.dataset.eventId === eventId)
   }
 }
