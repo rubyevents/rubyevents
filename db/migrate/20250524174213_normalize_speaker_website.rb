10467ebccbfc0ec50c9266c58d8dbe0ceabaefcd
Vinícius <vinicius.alonso@solides.com.br>
2025-05-24 16:08:39 -0300

    Remove edge case handle from migration

--- db/migrate/20250524174213_normalize_speaker_website.rb
+++ db/migrate/20250524174213_normalize_speaker_website.rb
@@ -1,9 +1,5 @@
 class NormalizeSpeakerWebsite < ActiveRecord::Migration[8.0]
   def change
-    # Fix an edge case where twitter and website are in the same field
-    speaker = Speaker.find_by(slug: "derrick-ko")
-    speaker.update(website: "http://derrickko.com", twitter: "http://twitter.com/derrickko")
-
     Speaker.where.not(website: [nil, ""]).find_in_batches do |speakers|
       speakers.each do |speaker|
         speaker.normalize_attribute(:website)
