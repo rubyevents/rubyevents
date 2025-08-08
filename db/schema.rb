ed130a9e78ad7803c704662353191a75fac205cf
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2025-08-08 15:38:30 -0400

    Strip Sponsor links params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_08_095915) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_08_192916) do
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
     t.integer "user_id"
