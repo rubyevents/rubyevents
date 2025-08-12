4fba5b0d960be965a595353974d23a6937e30058
DiegoPisa-Cedar <diego.pisa@cedarcode.com>
2025-08-12 15:02:31 -0300

    chore: Add tests for index action in SponsorsController

--- test/controllers/sponsors_controller_test.rb
+++ test/controllers/sponsors_controller_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
 class SponsorsControllerTest < ActionDispatch::IntegrationTest
+  fixtures :sponsors, :event_sponsors
+
   test "should get index" do
     get sponsors_url
     assert_response :success
+  end
+
+  test "should get index with sponsors ordered by name" do
+    get sponsors_url
+    assert_response :success
+    assert_equal sponsors(:two).name, assigns(:sponsors).first.name
+    assert_equal sponsors(:one).name, assigns(:sponsors).last.name
+  end
+
+  test "should get index with letter" do
+    get sponsors_url(letter: "a")
+    assert_response :success
+  end
+
+  test "should get index featured events" do
+    event_sponsor = event_sponsors(:one)
+    event_sponsor.update(sponsor: sponsors(:one))
+    event_sponsor.update(sponsor: sponsors(:two))
+    get sponsors_url
+    assert_response :success
+    assert_equal event_sponsor.sponsor.name, assigns(:featured_sponsors).first.name
+    assert_equal event_sponsor.sponsor.name, assigns(:featured_sponsors).last.name
   end
 
   test "should get show" do
