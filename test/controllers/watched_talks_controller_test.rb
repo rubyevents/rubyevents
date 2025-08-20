1a48384a62d1d81043bb272c6262a1c594d80d54
Justin Marsh <jmarsh24@gmail.com>
2025-08-21 00:18:38 +0200

    show only current users watched talks

--- test/controllers/watched_talks_controller_test.rb
+++ test/controllers/watched_talks_controller_test.rb
@@ -3,28 +3,7 @@ require "test_helper"
 class WatchedTalksControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    @other_user = users(:two)
-    @talk_one = talks(:one)
-    @talk_two = talks(:two)
-    @talk_three = talks(:three)
-
-    @watched_talk_one = watched_talks(:one)
-    @watched_talk_three = watched_talks(:three)
-    @watched_talk_two = watched_talks(:two)
-  end
-
-  test "should get index when authenticated" do
-    sign_in_as @user
-
-    get watched_talks_url
-    assert_response :success
-    assert_select "h1", /Recently Watched Videos/i
-    assert_equal @user.watched_talks.count, assigns(:talks).count
-  end
-
-  test "should redirect to sign in when not authenticated" do
-    get watched_talks_url
-    assert_redirected_to new_session_url
+    @user_two = users(:two)
   end
 
   test "should show only current user's watched talks" do
@@ -33,9 +12,9 @@ class WatchedTalksControllerTest < ActionDispatch::IntegrationTest
     get watched_talks_url
     assert_response :success
 
-    assert assigns(:talks).include?(@watched_talk_one)
-    assert assigns(:talks).include?(@watched_talk_three)
+    watched_talk_ids = assigns(:talks).map(&:talk_id)
+    user_watched_talk_ids = @user.watched_talks.pluck(:talk_id)
 
-    assert_not assigns(:talks).include?(@watched_talk_two)
+    assert_equal user_watched_talk_ids.sort, watched_talk_ids.sort
   end
 end
