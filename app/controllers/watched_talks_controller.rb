8d56d00f5e404191b271c2ff1c30928119bd082e
Justin Marsh <jmarsh24@gmail.com>
2025-08-31 14:42:08 +0200

    format

--- app/controllers/watched_talks_controller.rb
+++ app/controllers/watched_talks_controller.rb
@@ -6,7 +6,7 @@ class WatchedTalksController < ApplicationController
     @watched_talks = Current.user.watched_talks
       .includes(talk: [:speakers, {event: :organisation}, {child_talks: :speakers}])
       .order(created_at: :desc)
-    
+
     @talks = @watched_talks.map(&:talk)
   end
 
