25ad87f0b7d7c392d06d0d98538301ec7701e21e
Adrien Poly <adrienpoly@gmail.com>
2025-09-07 16:23:34 +0200

    wip

--- app/models/event_participation.rb
+++ app/models/event_participation.rb
@@ -31,7 +31,7 @@ class EventParticipation < ApplicationRecord
   validates :attended_as, presence: true, inclusion: {in: %w[speaker keynote_speaker visitor]}
 
   # enums
-  enum :attended_as, %w[keynote_speaker speaker visitor].index_by(&:itself), prefix: true
+  enum :attended_as, %w[organiser keynote_speaker speaker visitor].index_by(&:itself), prefix: true
 
   def name
     "#{user.name} - #{event.name} - #{attended_as}"
