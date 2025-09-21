1cf2a62928779895be4539d7135ba5a8524cbb38
Adrien Poly <adrienpoly@gmail.com>
2025-09-21 10:02:11 +0200

    optimize

--- db/migrate/20250920194438_add_country_code_to_event.rb
+++ db/migrate/20250920194438_add_country_code_to_event.rb
@@ -1,6 +1,6 @@
 class AddCountryCodeToEvent < ActiveRecord::Migration[8.1]
   def up
-    Event.all.each do |event|
+    Event.find_each.each do |event|
       event.update_column(:country_code, event.static_metadata&.country&.alpha2)
     end
   end
