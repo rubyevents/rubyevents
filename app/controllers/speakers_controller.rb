8e8afe92bb3ff53b9734f490965fc3a65df9c569
hschne <hello@hansschnedlitz.com>
2025-09-30 21:27:18 +0200

    Add speaker search

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -8,7 +8,6 @@ class SpeakersController < ApplicationController
     @speakers = User.speakers.order(:name)
     @speakers = @speakers.with_talks unless params[:with_talks] == "false"
     # @speakers = @speakers.canonical unless params[:canonical] == "false"
-    @speakers = @speakers.where("lower(name) LIKE ?", "#{params[:letter].downcase}%") if params[:letter].present?
     @speakers = @speakers.ft_search(params[:s]).with_snippets.ranked if params[:s].present?
     @pagy, @speakers = pagy(@speakers, gearbox_extra: true, gearbox_limit: [200, 300, 600], page: params[:page])
     respond_to do |format|
