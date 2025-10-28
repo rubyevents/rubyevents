3a5e21ced0c60cd11bbf2ff1007f54a145f131e3
Marco Roth <marco.roth@intergga.ch>
2025-10-28 23:15:40 +0100

    Update avo resource and add controller

--- app/avo/resources/alias.rb
+++ app/avo/resources/alias.rb
@@ -5,6 +5,7 @@ class Avo::Resources::Alias < Avo::BaseResource
     field :id, as: :id
     field :aliasable, as: :belongs_to, polymorphic_as: :aliasable, types: [::User, ::Event, ::Organisation, ::Sponsor]
     field :name, as: :text, required: true
+    field :slug, as: :text, required: true
     field :created_at, as: :date_time, readonly: true
     field :updated_at, as: :date_time, readonly: true
   end
