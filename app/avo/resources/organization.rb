b585902057dfdde80a253333edfe1bdf75e5b6d8
Marco Roth <marco.roth@intergga.ch>
2025-11-26 11:36:38 -0800

    Cleanup

--- app/avo/resources/organization.rb
+++ app/avo/resources/organization.rb
@@ -4,6 +4,13 @@ class Avo::Resources::Organization < Avo::BaseResource
   # self.search = {
   #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
   # }
+  self.find_record_method = -> {
+    if id.is_a?(Array)
+      query.where(slug: id)
+    else
+      query.find_by(slug: id)
+    end
+  }
 
   def fields
     field :id, as: :id
@@ -13,5 +20,7 @@ class Avo::Resources::Organization < Avo::BaseResource
     field :slug, as: :text
     field :description, as: :textarea
     field :main_location, as: :text
+    field :sponsors, as: :has_many
+    field :event_involvements, as: :has_many
   end
 end
