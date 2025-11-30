84704cd492325e4b5c1e5684c1118a7ab07a7153
Marco Roth <marco.roth@intergga.ch>
2025-11-30 19:16:53 +0100

    Add SpeakerdeckDeckTool

---
+++ app/tools/speakerdeck_deck_tool.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class SpeakerdeckDeckTool < RubyLLM::Tool
+  description "Fetch SpeakerDeck slide deck metadata by URL or username/slug using the oEmbed API"
+  param :url, desc: "Full SpeakerDeck URL (e.g., 'https://speakerdeck.com/username/slug') or path as 'username/slug'"
+
+  def execute(url:)
+    response = client.oembed(url)
+
+    {
+      url: response.url || url,
+      title: response.title,
+      author_name: response.author_name,
+      author_url: response.author_url,
+      provider_name: response.provider_name,
+      provider_url: response.provider_url,
+      width: response.width,
+      height: response.height,
+      ratio: response.ratio,
+      html: response.html
+    }
+  rescue => e
+    {error: e.message}
+  end
+
+  private
+
+  def client
+    @client ||= Speakerdeck::Client.new
+  end
+end
