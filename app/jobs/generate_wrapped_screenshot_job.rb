f6fa6be9c3cf6d0fc3b609ae2c37f184b73cdb33
Adrien Poly <adrienpoly@gmail.com>
2026-01-02 22:57:26 +0100

    limit concurrency to 1

--- app/jobs/generate_wrapped_screenshot_job.rb
+++ app/jobs/generate_wrapped_screenshot_job.rb
@@ -1,5 +1,7 @@
 class GenerateWrappedScreenshotJob < ApplicationJob
   queue_as :default
+  limits_concurrency to: 1, key: "generate_wrapped_screenshot", duration: 5.minutes
+
 
   def perform(user)
     User::WrappedScreenshotGenerator.generate_all(user)
