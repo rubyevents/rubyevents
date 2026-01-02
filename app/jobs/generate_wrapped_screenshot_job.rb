f02e83b1b928437eda83f878fbc7c40c49ac2cbc
Marco Roth <marco.roth@intergga.ch>
2026-01-03 00:07:20 +0100

    Standard

--- app/jobs/generate_wrapped_screenshot_job.rb
+++ app/jobs/generate_wrapped_screenshot_job.rb
@@ -2,7 +2,6 @@ class GenerateWrappedScreenshotJob < ApplicationJob
   queue_as :default
   limits_concurrency to: 1, key: "generate_wrapped_screenshot", duration: 5.minutes
 
-
   def perform(user)
     User::WrappedScreenshotGenerator.generate_all(user)
   end
