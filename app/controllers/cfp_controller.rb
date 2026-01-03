e4cb83d2be4858d1107e8854aa452d3089442299
Marco Roth <marco.roth@intergga.ch>
2026-01-03 12:15:07 +0100

    Add CFP filter for conferences/meetups

--- app/controllers/cfp_controller.rb
+++ app/controllers/cfp_controller.rb
@@ -3,6 +3,12 @@ class CFPController < ApplicationController
 
   # GET /cfp
   def index
-    @events = CFP.includes(:event).open.order(cfps: {close_date: :asc}).map(&:event)
+    cfps = CFP.includes(:event).open.order(cfps: {close_date: :asc})
+
+    if params[:kind].present? && params[:kind] != "all"
+      cfps = cfps.joins(:event).where(events: {kind: params[:kind]})
+    end
+
+    @events = cfps.map(&:event)
   end
 end
