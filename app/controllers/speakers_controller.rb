11bb727cbe86d21bd9dd1c8cf15832a751a228a1
Adrien Poly <adrienpoly@gmail.com>
2026-01-06 15:01:29 +0100

    prevent ambiguity error

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -5,9 +5,7 @@ class SpeakersController < ApplicationController
   # GET /speakers
   def index
     @speakers = User.speakers.order(:name)
-    @speakers = @speakers.with_talks unless params[:with_talks] == "false"
-    # @speakers = @speakers.canonical unless params[:canonical] == "false"
-    @speakers = @speakers.where("lower(name) LIKE ?", "#{params[:letter].downcase}%") if params[:letter].present?
+    @speakers = @speakers.where("lower(users.name) LIKE ?", "#{params[:letter].downcase}%") if params[:letter].present?
     @speakers = @speakers.ft_search(params[:s]).with_snippets.ranked if params[:s].present?
     @pagy, @speakers = pagy(@speakers, gearbox_extra: true, gearbox_limit: [200, 300, 600], page: params[:page])
 
