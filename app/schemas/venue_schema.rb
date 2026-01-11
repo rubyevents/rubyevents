10894fd93fef8e3d89b0dda21316c8e9eebe0b9b
Marco Roth <marco.roth@intergga.ch>
2026-01-11 02:23:52 +0100

    Allow openstreetmap key on hotels in venue.yml

--- app/schemas/venue_schema.rb
+++ app/schemas/venue_schema.rb
@@ -94,6 +94,7 @@ class VenueSchema < RubyLLM::Schema
       object :maps, required: false do
         string :google, description: "Google Maps URL", required: false
         string :apple, description: "Apple Maps URL", required: false
+        string :openstreetmap, description: "OpenStreetMap URL", required: false
       end
     end
   end
