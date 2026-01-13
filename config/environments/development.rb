a1003ff09583d8e32b29e08cd2ccfac39033cafc
Marco Roth <marco.roth@intergga.ch>
2026-01-13 14:49:25 +0100

    Configure `config.asset_host` for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Enable serving of images with full URLs
+  config.asset_host = "http://localhost:3000"
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
