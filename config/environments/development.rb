68f18cd8ce18781521d884fdd6db50fc6cac9249
Rachael Wright-Munn <chaelcodes@gmail.com>
2026-01-15 15:28:23 +0000

    Allow localhost to be accessed from a Codespace

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
 
   # Enable serving of images with full URLs
   config.asset_host = "http://localhost:3000"
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  config.asset_host = lambda { |source, request = nil|
+    request&.host&.include?("localhost") ? "http://localhost:3000" : "#{request&.protocol}#{request&.host_with_port}"
+  }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -81,6 +85,9 @@ Rails.application.configure do
   # config.generators.apply_rubocop_autocorrect_after_generate!
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # View local server when hosted by GitHub Codespaces.
+  config.hosts << /^[a-zA-Z0-9-]+-\d{4}\.app\.github\.dev$/
 
   # https://vite-ruby.netlify.app/guide/troubleshooting.html#safari-does-not-reflect-css-and-js-changes-in-development
   # https://bugs.webkit.org/show_bug.cgi?id=193533
