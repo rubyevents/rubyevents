1102ba645697b1d0dd0395ff948693fdf694e34e
Adrien Poly <adrienpoly@gmail.com>
2026-01-22 12:03:00 +0100

    alow to view draft article localy

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,14 +4,14 @@ class AnnouncementsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    announcements = Current.user&.admin? ? Announcement.all : Announcement.published
+    announcements = can_view_draft_articles? ? Announcement.all : Announcement.published
     @pagy, @announcements = pagy_array(announcements, limit: 10, page: page_number)
   end
 
   def show
     @announcement = Announcement.find_by_slug!(params[:slug])
 
-    unless @announcement.published? || Current.user&.admin?
+    unless @announcement.published? || can_view_draft_articles?
       raise ActiveRecord::RecordNotFound
     end
 
@@ -33,5 +33,9 @@ class AnnouncementsController < ApplicationController
 
   def page_number
     [params[:page]&.to_i, 1].compact.max
+  end
+
+  def can_view_draft_articles?
+    Current.user&.admin? || Rails.env.development? || params[:preview] == "true"
   end
 end
