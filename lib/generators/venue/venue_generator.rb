bae1fa9c1d175f816b5459c2240b203d1f1e9f0f
Rachael Wright-Munn <hello@chael.codes>
2026-01-22 23:02:51 +0000

    Update Geocoder to main which now has a street_address on Nominatim Result.

--- lib/generators/venue/venue_generator.rb
+++ lib/generators/venue/venue_generator.rb
@@ -14,7 +14,7 @@ class VenueGenerator < Generators::EventBase
   class_option :rooms, type: :boolean, desc: "Include rooms section", default: false, group: "Fields"
   class_option :spaces, type: :boolean, desc: "Include spaces section", default: false, group: "Fields"
 
-  GeocodedAddress = Struct.new(:address, :city, :state, :postal_code, :country, :country_code, :latitude, :longitude)
+  GeocodedAddress = Struct.new(:street_address, :city, :state, :postal_code, :country, :country_code, :latitude, :longitude)
 
   def copy_venue_file
     geocode_address
@@ -22,16 +22,20 @@ class VenueGenerator < Generators::EventBase
   end
 
   def geocode_address
-    if options[:address] != "123 Main St, City, State, ZIP, Country" && options[:venue_name] != "Korjaamo Kino cinema"
-      search = [options[:venue_name], options[:address]].compact.join(", ")
+    if options[:address] != "123 Main St, City, State, ZIP, Country" || options[:name] != "Venue name"
+      # Combine venue name and address for better accuracy
+      search = [options[:name], options[:address]].compact.join(", ")
       geocode_results = Geocoder.search(search)
+      # Nominatim works better with separate queries - doesn't find the combined one
       geocode_results = Geocoder.search(options[:address]) if geocode_results.empty?
-      geocode_results = Geocoder.search(options[:venue_name]) if geocode_results.empty?
+      geocode_results = Geocoder.search(options[:name]) if geocode_results.empty?
+      # Nominatim works better with just the street address
+      geocode_results = Geocoder.search(options[:address].split(",")[0]) if geocode_results.empty?
     end
 
     @geocoded_address = geocode_results&.first ||
       GeocodedAddress.new(
-        address: "123 Main St",
+        street_address: "123 Main St",
         city: "City",
         state: "State",
         postal_code: "ZIP",
