bbbfd93053dc338ebbb04cbfd2d190d98060490d
Matt Mayer <matt@matthewmayer.co.uk>
2026-02-03 12:56:53 +0700

    sort organizations case-insensitive

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,7 +4,7 @@ class OrganizationsController < ApplicationController
 
   # GET /organizations
   def index
-    @organizations = Organization.includes(:events).order(:name)
+    @organizations = Organization.includes(:events).order("LOWER(name)")
     @organizations = @organizations.where("lower(name) LIKE ?", "#{params[:letter].downcase}%") if params[:letter].present?
     @featured_organizations = Organization.joins(:sponsors).group("organizations.id").order("COUNT(sponsors.id) DESC").limit(25).includes(:events)
   end
