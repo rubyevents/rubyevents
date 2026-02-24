53faecea1d8db652cb5dbb1f1a6db11922428e11
Vinicius <contato@viniciusalonso.com>
2026-02-24 19:00:56 -0300

    Order by series name

--- app/controllers/events/archive_controller.rb
+++ app/controllers/events/archive_controller.rb
@@ -2,7 +2,7 @@ class Events::ArchiveController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
 
   def index
-    @events = Event.canonical.includes(:series).order("events.start_date ASC, events.name ASC")
+    @events = Event.canonical.includes(:series).order("series.name ASC, events.start_date ASC")
     @events = @events.where("lower(events.name) LIKE ?", "#{params[:letter].downcase}%") if params[:letter].present?
     @events = @events.ft_search(params[:s]) if params[:s].present?
   end
