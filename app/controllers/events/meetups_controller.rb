07179b6a03aa4ed941a164a44377b4c78e870c78
Matt Mayer <matt@matthewmayer.co.uk>
2026-03-15 22:50:59 +0700

    Add browse by country section to meetups page, and request for contributions
    * adds browse by country (filtered to only countries with meetups, and linking to their meetups tab)
    * links to continents, countries and cities pages
    * banner to request more data for meetups (based on same design as the one for organizations)

--- app/controllers/events/meetups_controller.rb
+++ app/controllers/events/meetups_controller.rb
@@ -1,7 +1,7 @@
 class Events::MeetupsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
-  # GET /meetups
+  # GET /events/meetups
   def index
     @meetups = Event.where(kind: :meetup)
       .joins(:talks)
@@ -9,5 +9,20 @@ class Events::MeetupsController < ApplicationController
       .includes(:series)
       .group("events.id")
       .order("max(talks.date) DESC")
+
+    @countries_by_continent = Event.distinct
+      .where(kind: :meetup)
+      .where.not(country_code: [nil, ""])
+      .pluck(:country_code)
+      .filter_map { |code| Country.find_by(country_code: code) }
+      .group_by(&:continent)
+      .sort_by { |continent, _| continent&.name || "ZZ" }
+      .to_h
+
+    @events_by_country = Event.includes(:series)
+      .where(kind: :meetup)
+      .where.not(country_code: [nil, ""])
+      .grouped_by_country
+      .to_h
   end
 end
