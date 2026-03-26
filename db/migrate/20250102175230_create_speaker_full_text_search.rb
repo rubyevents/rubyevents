4997c12f79751d3a2fd3bcc2884d291051729017
Rachael Wright-Munn <hello@chael.codes>
2026-03-26 01:02:19 +0000

    Updates to Docker and Devcontainers (#1456)

--- db/migrate/20250102175230_create_speaker_full_text_search.rb
+++ db/migrate/20250102175230_create_speaker_full_text_search.rb
@@ -3,8 +3,6 @@ class CreateSpeakerFullTextSearch < ActiveRecord::Migration[8.0]
     create_virtual_table :speakers_search_index, :fts5, [
       "name", "github", "tokenize = porter"
     ]
-
-    Speaker.reindex_all
   end
 
   def down
