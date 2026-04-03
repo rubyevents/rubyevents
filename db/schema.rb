42addd154fcf8c364c481161e1c7da1503530a21
Vinicius <contato@viniciusalonso.com>
2026-04-03 01:12:47 -0300

    Create NotificationUser model

--- db/schema.rb
+++ db/schema.rb
@@ -278,6 +278,18 @@ ActiveRecord::Schema[8.2].define(version: 2026_03_06_110802) do
     t.index ["user_id"], name: "index_notification_user_subscriptions_on_user_id"
   end
 
+  create_table "notification_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "notification_id", null: false
+    t.string "object_class", null: false
+    t.integer "object_id", null: false
+    t.boolean "read", default: false, null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["notification_id"], name: "index_notification_users_on_notification_id"
+    t.index ["user_id"], name: "index_notification_users_on_user_id"
+  end
+
   create_table "notifications", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -585,6 +597,8 @@ ActiveRecord::Schema[8.2].define(version: 2026_03_06_110802) do
   add_foreign_key "favorite_users", "users", column: "favorite_user_id"
   add_foreign_key "notification_user_subscriptions", "notifications"
   add_foreign_key "notification_user_subscriptions", "users"
+  add_foreign_key "notification_users", "notifications"
+  add_foreign_key "notification_users", "users"
   add_foreign_key "password_reset_tokens", "users"
   add_foreign_key "sessions", "users"
   add_foreign_key "speakers", "speakers", column: "canonical_id"
