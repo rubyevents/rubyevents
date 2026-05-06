98a22b8b25c26781991723d9d7de4587aae6f4c3
David Hill <david.esmale@gmail.com>
2026-05-06 12:59:24 -0400

    move  into the body of the helper, instead of as a keyword argument

--- app/helpers/events/schedules_helper.rb
+++ app/helpers/events/schedules_helper.rb
@@ -1,5 +1,6 @@
 module Events::SchedulesHelper
-  def selected_schedule_day(days, today: Time.zone.today.to_s)
+  def selected_schedule_day(days)
+    today = Time.zone.today.to_s
     days.detect { |day| day["date"] == today } || days.first
   end
 end
