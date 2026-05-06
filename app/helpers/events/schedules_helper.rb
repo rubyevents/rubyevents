9b4193704189261671a710ff8cd93be60e992821
David Hill <david.esmale@gmail.com>
2026-05-06 07:50:34 -0400

    add .to_s to  to ensure comparing strings, instead of string vs date

--- app/helpers/events/schedules_helper.rb
+++ app/helpers/events/schedules_helper.rb
@@ -1,5 +1,5 @@
 module Events::SchedulesHelper
-  def selected_schedule_day(days, today: Time.zone.today)
+  def selected_schedule_day(days, today: Time.zone.today.to_s)
     days.detect { |day| day["date"] == today } || days.first
   end
 end
