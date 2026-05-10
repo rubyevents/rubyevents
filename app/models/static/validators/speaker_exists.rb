196f1fadcccf0ef7686da37cf12ad55d6c386ff8
Rachael Wright-Munn <hello@chael.codes>
2026-05-10 19:36:25 +0000

    Note the location of errors in speaker exists.

--- app/models/static/validators/speaker_exists.rb
+++ app/models/static/validators/speaker_exists.rb
@@ -28,34 +28,52 @@ module Static
         errors = []
         document = Yerba.parse_file(@file_path)
 
-        Array(document).each_with_index do |video, index|
-
-          Array(document["[#{index}]"]["speakers"]).each_with_index do |name, index|
-            unless KNOWN_NAMES.include?(name.to_s)
-              binding.irb
-              location = document["[#{index}]"]["speakers"][index.to_s]&.location
-
-              errors << Static::Validators::Error.new(
-                "Speaker '#{name}' not found in speakers.yml",
-                file_path: @file_path,
-                line: location&.start_line || 1,
-                end_line: location&.end_line
-              )
-            end
+        index = 0
+        loop do
+          speakers = document.at_path("[].speakers[#{index}]")
+          break if speakers.empty?
+          speakers.each do |speaker|
+            name = speaker&.value
+            next if name.blank? || KNOWN_NAMES.include?(name)
+            location = speaker&.location
+            errors << Static::Validators::Error.new(
+              "Speaker '#{name}' not found in speakers.yml",
+              file_path: @file_path,
+              line: location&.start_line || 1,
+              end_line: location&.end_line
+            )
           end
+          index += 1
+        end
 
-          Array(video["talks"]).each do |talk|
-            Array(talk["speakers"]).each do |name|
-              unless KNOWN_NAMES.include?(name)
-                errors << Static::Validators::Error.new(
-                  "Speaker '#{name}' not found in speakers.yml",
-                  file_path: @file_path,
-                  line: 1,
-                  end_line: 1
-                )
-              end
-            end
+        video_index, talk_index, speakers_index = 0, 0, 0
+        loop do
+          videos = document["[#{video_index}]"]
+          talks = document["[#{video_index}].talks[#{talk_index}]"]
+          talk_speakers = document["[#{video_index}].talks[#{talk_index}].speakers[#{speakers_index}]"]
+          if videos.nil?
+            break
+          elsif talks.nil?
+            video_index += 1
+            talk_index = 0
+            speakers_index = 0
+            next
+          elsif talk_speakers.nil?
+            talk_index += 1
+            speakers_index = 0
+            next
           end
+          name = talk_speakers.value
+          unless name.blank? || KNOWN_NAMES.include?(name)
+            location = talk_speakers.location
+            errors << Static::Validators::Error.new(
+              "Speaker '#{name}' not found in speakers.yml",
+              file_path: @file_path,
+              line: location&.start_line || 1,
+              end_line: location&.end_line
+            )
+          end
+          speakers_index += 1
         end
 
         errors
