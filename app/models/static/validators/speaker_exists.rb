688fb16bae37e09c3383e444e808b71bda8bc505
Marco Roth <marco.roth@intergga.ch>
2026-05-13 02:53:12 +0200

    Update tests and add `#applicable?` and `#errors` back

--- app/models/static/validators/speaker_exists.rb
+++ app/models/static/validators/speaker_exists.rb
@@ -3,6 +3,26 @@
 module Static
   module Validators
     class SpeakerExists
+      PATTERNS = ["**/videos.yml"].freeze
+
+      def initialize(file_path:)
+        @file_path = file_path.to_s.sub("#{Rails.root}/", "")
+      end
+
+      def applicable?
+        return false unless File.exist?(@file_path)
+
+        PATTERNS.any? do |pattern|
+          File.fnmatch?(pattern, @file_path, File::FNM_PATHNAME)
+        end
+      end
+
+      def errors
+        return [] unless applicable?
+
+        self.class.errors.select { |e| e.file_path == @file_path }
+      end
+
       def self.errors
         @errors ||= Static::SpeakersFile.new.missing_speaker_references.map do |scalar|
           Static::Validators::Error.new(
