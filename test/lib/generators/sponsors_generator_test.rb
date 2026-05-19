7426d0992b07c61574cc84afaab8987c61062fb1
Rachael Wright-Munn <hello@chael.codes>
2026-05-19 17:00:44 +0000

    SponsorsGenerator can mutate existing files.
    The existing SponsorsGenerator expected you to provide all sponsors and
    their tiers as arguments, and it would generate a new sponsors.yml file
    based on that input.
    
    This meant that if you wanted to add a new sponsor or update an existing
    sponsor's tier, you had to provide the full list of sponsors again.
    
    Adding new sponsors happens frequently. Now we use Yerba to add sponsors
    to an existing file, so you can add them one at a time.

--- test/lib/generators/sponsors_generator_test.rb
+++ test/lib/generators/sponsors_generator_test.rb
@@ -7,28 +7,127 @@ class SponsorsGeneratorTest < Rails::Generators::TestCase
   destination Rails.root.join("tmp/generators/sponsors")
   setup :prepare_destination
 
-  test "generator populates sponsors.yml with arguments" do
+  test "generator creates an empty sponsors file with specified tiers" do
     assert_nothing_raised do
-      run_generator ["typesense:platinum", "braze:platinum", "AppSignal:Gold", "--event_series", "tropicalrb", "--event", "tropical-on-rails-2026"]
+      run_generator ["--tiers", "Platinum,Gold,Silver",
+        "--event", "tropical-on-rails-2026"]
     end
+    file_path = File.join(destination_root, "data/tropicalrb/tropical-on-rails-2026/sponsors.yml")
 
     assert_file "data/tropicalrb/tropical-on-rails-2026/sponsors.yml" do |content|
-      assert_match(/platinum/, content, "platinum Tier missing")
+      assert_match(/name: "Platinum"/, content, "Platinum Tier missing")
       assert_match(/Gold/, content, "Gold Tier missing")
-      assert_match(/typesense/, content, "typesense sponsor missing")
-      assert_match(/appsignal/, content, "AppSignal sponsor missing")
+      assert_match(/Silver/, content, "Silver Tier missing")
     end
 
-    File.delete File.join(destination_root, "data/tropicalrb/tropical-on-rails-2026/sponsors.yml")
+    sponsors_file_passes_validations(file_path:)
+
+    File.delete file_path
   end
 
-  test "sponsors.yml passes schema validation" do
-    run_generator ["--event-series", "rubyconf", "--event", "2024"]
+  test "generator creates a sponsors.yml and adds a first sponsor" do
+    skip "BLOCKED - cannot add to empty array"
+    assert_nothing_raised do
+      run_generator ["--tiers", "Platinum,Gold,Silver",
+        "--event", "tropical-on-rails-2026",
+        "--name", "Typesense",
+        "--website", "https://typesense.org",
+        "--logo-url", "https://typesense.org/logo.png",
+        "--tier", "Platinum"]
+    end
+    file_path = File.join(destination_root, "data/tropicalrb/tropical-on-rails-2026/sponsors.yml")
 
-    sponsor_file_path = File.join(destination_root, "data/rubyconf/2024/sponsors.yml")
-    errors = Static::Validators::SchemaArray.new(file_path: sponsor_file_path).validate
+    assert_file "data/tropicalrb/tropical-on-rails-2026/sponsors.yml" do |content|
+      assert_match(/name: "Platinum"/, content, "Platinum Tier missing")
+      assert_match(/Gold/, content, "Gold Tier missing")
+      assert_match(/Silver/, content, "Silver Tier missing")
+      assert_match(/Typesense/, content, "Typesense sponsor missing")
+      assert_match(/https:\/\/typesense.org/, content, "typesense website missing")
+      assert_match(/https:\/\/typesense.org\/logo.png/, content, "typesense logo URL missing")
+    end
+
+    sponsors_file_passes_validations(file_path:)
+
+    File.delete file_path
+  end
+
+  test "generator adds a new sponsor to a tier with existing sponsors" do
+    skip "Cannot generate file with empty sponsors array yet, so this test depends on the previous one passing :["
+    run_generator ["--tiers", "Platinum,Gold,Silver",
+      "--event", "tropical-on-rails-2026",
+      "--name", "Typesense",
+      "--website", "https://typesense.org",
+      "--logo-url", "https://typesense.org/logo.png",
+      "--tier", "Platinum"]
+    file_path = File.join(destination_root, "data/tropicalrb/tropical-on-rails-2026/sponsors.yml")
+
+    assert_file "data/tropicalrb/tropical-on-rails-2026/sponsors.yml" do |content|
+      assert_match(/Typesense/, content, "Typesense sponsor not added to the correct tier")
+    end
+
+    run_generator ["--event", "tropical-on-rails-2026",
+      "--name", "Braze",
+      "--website", "https://braze.com",
+      "--logo-url", "https://braze.com/logo.png",
+      "--tier", "Platinum"]
+
+    assert_file "data/tropicalrb/tropical-on-rails-2026/sponsors.yml" do |content|
+      assert_match(/Braze/, content, "Braze sponsor not added")
+      assert_match(/Typesense/, content, "Typesense sponsor removed")
+    end
+
+    sponsors_file_passes_validations(file_path:)
+
+    File.delete file_path
+  end
+
+  test "generator updates an existing sponsor's information" do
+    skip "Coming soon"
+
+    run_generator ["--tiers", "Platinum,Gold,Silver",
+      "--event", "tropical-on-rails-2026",
+      "--name", "Typesense",
+      "--website", "https://typesense.org",
+      "--logo-url", "https://typesense.org/logo.png",
+      "--tier", "Gold"]
+    file_path = File.join(destination_root, "data/tropicalrb/tropical-on-rails-2026/sponsors.yml")
+
+    assert_file "data/tropicalrb/tropical-on-rails-2026/sponsors.yml" do |content|
+      assert_match(/Typesense/, content, "Typesense sponsor not added")
+    end
+
+    run_generator ["--event", "tropical-on-rails-2026",
+      "--name", "Typesense",
+      "--tier", "Platinum"]
+
+    assert_file "data/tropicalrb/tropical-on-rails-2026/sponsors.yml" do |content|
+      assert_match(/Typesense/, content, "Typesense sponsor not found in Platinum tier")
+    end
+
+    sponsors_file_passes_validations(file_path:)
+
+    File.delete file_path
+  end
+
+  test "generator pulls sponsor information from other sponsor files" do
+    skip "Implement LATER :)"
+    run_generator ["--tiers", "Platinum,Gold,Silver",
+      "--event", "tropical-on-rails-2026", "--name", "Typesense", "--tier", "Gold"]
+    file_path = File.join(destination_root, "data/tropicalrb/tropical-on-rails-2026/sponsors.yml")
+
+    assert_file "data/tropicalrb/tropical-on-rails-2026/sponsors.yml" do |content|
+      assert_match(/Typesense/, content, "Typesense sponsor not found in Gold tier")
+      assert_match(/https:\/\/typesense.org/, content, "Typesense website not found")
+      assert_match(/https:\/\/typesense.org\/logo.png/, content, "Typesense logo URL not found")
+    end
+
+    sponsors_file_passes_validations(file_path:)
+
+    File.delete file_path
+  end
+
+  def sponsors_file_passes_validations(file_path:)
+    errors = Static::Validators::SchemaArray.new(file_path: file_path).validate
     assert_empty errors, "Sponsors YAML does not conform to schema: #{errors.join(", ")}"
-
-    File.delete sponsor_file_path
   end
 end
