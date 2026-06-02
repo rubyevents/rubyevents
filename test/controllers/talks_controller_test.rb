cd267dc832086424779fa9cd0d0501887391f218
Marco Roth <marco.roth@intergga.ch>
2026-06-02 15:48:38 +0200

    Remove functionality to edit talks

--- test/controllers/talks_controller_test.rb
+++ test/controllers/talks_controller_test.rb
@@ -62,55 +62,6 @@ class TalksControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to talk_path(@talk)
   end
 
-  test "should get edit" do
-    get edit_talk_url(@talk), headers: {"Turbo-Frame" => "modal"}
-    assert_response :success
-  end
-
-  test "anonymous user can suggest a modification" do
-    patch talk_url(@talk), params: {talk: {description: "new description", slug: "new-slug", title: "new title", date: "2024-01-01"}}
-    assert_redirected_to talk_url(@talk)
-    assert_equal "Your suggestion was successfully created and will be reviewed soon.", flash[:notice]
-    assert_equal 1, @talk.suggestions.pending.count
-    assert_not_equal "new description", @talk.reload.description
-    assert_not_equal "new title", @talk.title
-    assert_not_equal "2024-01-01", @talk.date.to_s
-  end
-
-  test "admin user can update talk" do
-    @user = users(:admin)
-    sign_in_as @user
-
-    patch talk_url(@talk), params: {talk: {summary: "new summary", description: "new description", slug: "new-slug", title: "new title", date: "2024-01-01"}}
-    assert_redirected_to talk_url(@talk)
-    assert_equal "Modification approved!", flash[:notice]
-    assert_equal 1, @talk.suggestions.approved.count
-    assert_equal "new description", @talk.reload.description
-    assert_equal "new summary", @talk.summary
-    assert_equal "new title", @talk.title
-    assert_equal "2024-01-01", @talk.date.to_s
-
-    # some attributes cannot be changed
-    assert_not_equal "new-slug", @talk.slug
-  end
-
-  test "owner can update directly the talk" do
-    user = @talk.users.first
-    sign_in_as user
-
-    patch talk_url(@talk), params: {talk: {summary: "new summary", description: "new description", slug: "new-slug", title: "new title", date: "2024-01-01"}}
-    assert_redirected_to talk_url(@talk)
-    assert_equal "Modification approved!", flash[:notice]
-    assert_equal 1, @talk.suggestions.approved.count
-    assert_equal "new description", @talk.reload.description
-    assert_equal "new summary", @talk.summary
-    assert_equal "new title", @talk.title
-    assert_equal "2024-01-01", @talk.date.to_s
-
-    # some attributes cannot be changed
-    assert_not_equal "new-slug", @talk.slug
-  end
-
   test "should show topics" do
     # to remove when we remove the poor man FF for the topics
     @user = users(:admin)
