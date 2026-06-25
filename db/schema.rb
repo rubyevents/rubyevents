5c91f1995e280a4a2c4498f2fc1e23ee71a698c2
Marco Roth <marco.roth@intergga.ch>
2026-06-25 11:40:14 +0100

    Dedupe duplicate event participation records

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.2].define(version: 2026_06_23_000500) do
+ActiveRecord::Schema[8.2].define(version: 2026_06_25_000000) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
