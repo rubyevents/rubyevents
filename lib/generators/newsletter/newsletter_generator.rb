e31ac29d3b04853c2844d0d5c2dd215c01d221b8
Rachael Wright-Munn <hello@chael.codes>
2026-07-02 01:01:57 +0000

    Newsletter generator (#1839)
    Co-authored-by: Marco Roth <marco.roth@intergga.ch>

---
+++ lib/generators/newsletter/newsletter_generator.rb
@@ -0,0 +1,47 @@
+class NewsletterGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("templates", __dir__)
+
+  class_option :year, type: :string, default: Time.now.year.to_s, desc: "Year for the newsletter"
+
+  def create_newsletter_file
+    @month = name.capitalize
+    @year = options[:year]
+    @first_day_of_month = Date.new(@year.to_i, Date::MONTHNAMES.index(@month), 1)
+    @last_day_of_month = @first_day_of_month.end_of_month
+    @github_date_range = "#{@first_day_of_month.iso8601}..#{@last_day_of_month.iso8601}"
+    # Newsletter releases on the first day of the next month.
+    @newsletter_date = @first_day_of_month.next_month
+
+    @next_month = @newsletter_date.strftime("%B")
+
+    @cfps = CFP.where("close_date >= ?", @newsletter_date).order(:close_date)
+
+    @this_month_events = Event.where(start_date: @first_day_of_month..@last_day_of_month).or(Event.where(end_date: @first_day_of_month..@last_day_of_month)).order(:start_date)
+
+    @next_month_events = Event.where(start_date: @newsletter_date..@newsletter_date.end_of_month).or(Event.where(end_date: @newsletter_date..@newsletter_date.end_of_month)).order(:start_date)
+
+    filename = "#{@newsletter_date}-#{@month.downcase}-newsletter.md"
+    @filepath = File.join("content", "announcements", filename)
+
+    template "template.md.tt", @filepath
+  end
+
+  private
+
+  def fetch_this_months_contributors
+    prs = JSON.parse(%x(
+        gh pr list --repo RubyEvents/RubyEvents --limit 100 \
+          --state merged --search "merged:#{@github_date_range}" \
+          --json author,number,url
+      ))
+    prs.group_by { it["author"] }
+  end
+
+  def event_dates_in_sentence(event)
+    if event.start_date == event.end_date
+      "on #{event.formatted_dates}"
+    else
+      "from #{event.formatted_dates}"
+    end.gsub(/, #{@year}/, "")
+  end
+end
