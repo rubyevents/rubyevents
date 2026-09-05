58b5e3bae24a87316a5ac2378b04a7a19dab267e
Marco Roth <marco.roth@intergga.ch>
2026-09-05 11:01:01 +0200

    Add RubyKaigi 2026 recordings (#2099)

--- test/clients/youtube/playlist_items_test.rb
+++ test/clients/youtube/playlist_items_test.rb
@@ -8,4 +8,16 @@ class YouTube::PlaylistItemsTest < ActiveSupport::TestCase
       assert items.length > 50
     end
   end
+
+  test "published_at is when the video went live, not when it was added to the playlist" do
+    VCR.use_cassette("youtube/playlist_items/all", match_requests_on: [:method]) do
+      items = YouTube::PlaylistItems.new.all(playlist_id: "PLE7tQUdRKcyZYz0O3d9ZDdo0-BkOWhrSk")
+
+      item = items.find { |i| i.video_id == "-ExPO-FCKQA" }
+      assert_equal "2023-03-01T16:00:00Z", item.published_at
+
+      assert_operator items.map(&:published_at).uniq.length, :>, 1,
+        "every item got the same published_at, which means the playlist-add time leaked back in"
+    end
+  end
 end
