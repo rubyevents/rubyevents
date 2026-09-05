f376cd53c33c6da89a743212d8a413bd8a31ab37
Marco Roth <marco.roth@intergga.ch>
2026-09-05 10:22:45 +0200

    Add RubyKaigi 2026 recordings

--- app/clients/youtube/playlist_items.rb
+++ app/clients/youtube/playlist_items.rb
@@ -2,13 +2,15 @@ module YouTube
   class PlaylistItems < YouTube::Client
     def all(playlist_id:)
       all_items("/playlistItems", query: {playlistId: playlist_id, part: "snippet,contentDetails"}).map do |metadata|
+        published_at = metadata.contentDetails&.videoPublishedAt || metadata.snippet.publishedAt
+
         OpenStruct.new({
           id: metadata.id,
           title: metadata.snippet.title,
           description: metadata.snippet.description,
-          published_at: DateTime.parse(metadata.snippet.publishedAt).to_date.to_s,
+          published_at: DateTime.parse(published_at).utc.strftime("%Y-%m-%dT%H:%M:%SZ"),
           channel_id: metadata.snippet.channelId,
-          year: metadata.snippet.title.match(/\d{4}/).to_s.presence || DateTime.parse(metadata.snippet.publishedAt).year,
+          year: metadata.snippet.title.match(/\d{4}/).to_s.presence || DateTime.parse(published_at).year,
           slug: metadata.snippet.title.parameterize,
           thumbnail_xs: metadata.snippet.thumbnails.default&.url,
           thumbnail_sm: metadata.snippet.thumbnails.medium&.url,
