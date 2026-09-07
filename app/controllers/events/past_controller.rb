98741529895fbb33863e662ffc04ff0cb2ae49c7
Dennis Paagman <dennis@paagman.dev>
2026-09-07 22:06:55 +0200

    Add continent filter pills to event collections (#2032)

--- app/controllers/events/past_controller.rb
+++ app/controllers/events/past_controller.rb
@@ -1,10 +1,11 @@
 class Events::PastController < ApplicationController
+  include ContinentFilterable
+
   skip_before_action :authenticate_user!, only: %i[index]
 
   def index
-    @events = Event.includes(:series, :keynote_speakers)
-      .where(end_date: ...Date.today)
-      .order(start_date: :desc)
-      .limit(50)
+    @events = filter_by_continent(
+      Event.includes(:series, :keynote_speakers).where(end_date: ...Date.today)
+    ).order(start_date: :desc).limit(50)
   end
 end
